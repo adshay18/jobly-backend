943f16dbc16355f09063653b4c76b86fc884bc61
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-13 10:41:03 -0400

    retry bcrypt

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const db = require('../db');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const { sqlForPartialUpdate } = require('../helpers/sql');
 const { NotFoundError, BadRequestError, UnauthorizedError } = require('../expressError');
 
